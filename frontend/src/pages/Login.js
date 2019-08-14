2ab6c8421cb7a1e3dd32391a532f6a77d86193ff
Artur Brasil <arturmbrasil@hotmail.com>
2019-08-13 23:56:16 -0300

    tindev completo

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+import api from "../services/api";
+
+import logo from "../assets/logo.svg";
+
+export default function Login({ history }) {
+  const [username, setUsername] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      username: username
+    });
+
+    console.log(response.data);
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  async function handleKey(e) {
+    if (e.key === "Enter") {
+      const response = await api.post("/devs", {
+        username: username
+      });
+
+      const { _id } = response.data;
+
+      history.push(`/dev/${_id}`);
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          placeholder="Digite seu usuário do GitHub"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          onKeyPress={handleKey}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
